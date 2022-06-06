10a5bff992ada1cb3bbaac28aba801c9348ecdc6
hayoung <>
2022-06-06 17:26:06 +0900

    style: 코드 공백 제거

--- src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
+++ src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
 @SpringBootApplication
 @EnableScheduling
 public class WebsocketDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebsocketDemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketDemoApplication.class, args);
+    }
 }
