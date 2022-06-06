dec7dae7a2defe7441b2f4587fb7484880402b8d
hayoung <>
2022-06-06 17:24:01 +0900

    refactor: event listener 패키지 변경

--- src/main/java/com/example/websocketdemo/listener/WebSocketEventListener.java
+++ src/main/java/com/example/websocketdemo/listener/WebSocketEventListener.java
@@ -1,15 +1,12 @@
-package com.example.websocketdemo.controller;
+package com.example.websocketdemo.listener;
 
 import com.example.websocketdemo.model.ChatMessage;
-import com.example.websocketdemo.model.ChatRoom;
 import com.example.websocketdemo.service.ChatRoomService;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-
 
 @Component
 public class WebSocketEventListener {
@@ -26,20 +23,17 @@ public class WebSocketEventListener {
 
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
 
-        String username = (String)headerAccessor.getSessionAttributes().get("username");
-        String chatRoomId = (String)headerAccessor.getSessionAttributes().get("chatRoomId");
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        String chatRoomId = (String) headerAccessor.getSessionAttributes().get("chatRoomId");
 
-        if(username != null && chatRoomId != null) {
-            ChatRoom oldChatRoom = chatRoomService.findById(chatRoomId);
-            oldChatRoom.minCount();
-            ChatRoom chatRoom = new ChatRoom(oldChatRoom.getId(), oldChatRoom.getName(), oldChatRoom.getCount(), oldChatRoom.getExpiryDate());
-            chatRoomService.save(chatRoom);
+        if (username != null && chatRoomId != null) {
+            chatRoomService.exit(chatRoomId);
 
             ChatMessage chatMessage = new ChatMessage();
             chatMessage.setType(ChatMessage.MessageType.LEAVE);
             chatMessage.setSender(username);
 
-            messagingTemplate.convertAndSend("/topic/chat/"+chatRoomId, chatMessage);
+            messagingTemplate.convertAndSend("/topic/chat/" + chatRoomId, chatMessage);
         }
     }
 }
