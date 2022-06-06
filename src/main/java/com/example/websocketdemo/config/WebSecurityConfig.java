e553f4f79e674d8aa3aad35b4934efca82b9810d
hayoung <>
2022-06-06 17:16:42 +0900

    chore: security 설정 수정

--- src/main/java/com/example/websocketdemo/config/WebSecurityConfig.java
+++ src/main/java/com/example/websocketdemo/config/WebSecurityConfig.java
@@ -8,13 +8,11 @@ import com.example.websocketdemo.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -35,17 +33,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers(HttpMethod.POST, "/api/v1/users/**");
-        web.ignoring().antMatchers("/ws/**", "/", "/join", "/login", "/js/**", "/css/**", "/error");
+        web.ignoring().antMatchers("/ws/**", "/", "/join", "/login", "/js/**", "/css/**", "/error", "/h2-console/**");
     }
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http
-                .cors()
-                .and()
-                .csrf().disable()
+        http.
+                csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler())
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -53,20 +49,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
-                .addFilterAfter(new JwtAuthenticationFilter(refreshTokenService, jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+                .addFilterAfter(new JwtAuthenticationFilter(jwtTokenProvider, refreshTokenService), UsernamePasswordAuthenticationFilter.class);
     }
 
 
     @Bean
     public AuthenticationEntryPoint unauthorizedHandler() {
         return new AuthEntryPointJwt();
-    }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception { // 9
-        auth.userDetailsService(userService)
-                // 해당 서비스(userService)에서는 UserDetailsService를 implements해서
-                // loadUserByUsername() 구현해야함 (서비스 참고)
-                .passwordEncoder(new BCryptPasswordEncoder());
     }
 }
