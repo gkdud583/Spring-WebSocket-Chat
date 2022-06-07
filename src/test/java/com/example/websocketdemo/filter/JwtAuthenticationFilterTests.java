14fd6b06e782ba950b179754941ea0ed9e113cd2
hayoung <>
2022-06-07 20:52:47 +0900

    refactor: Jwt 인증 처리 필터 테스트 수정

--- src/test/java/com/example/websocketdemo/filter/JwtAuthenticationFilterTests.java
+++ src/test/java/com/example/websocketdemo/filter/JwtAuthenticationFilterTests.java
@@ -1,31 +1,22 @@
 package com.example.websocketdemo.filter;
 
-import com.example.websocketdemo.entity.UserInfo;
-import com.example.websocketdemo.model.RefreshToken;
-import com.example.websocketdemo.provider.JwtTokenProvider;
+import com.example.websocketdemo.entity.RefreshToken;
+import com.example.websocketdemo.jwt.JwtTokenProvider;
 import com.example.websocketdemo.service.RefreshTokenService;
-import com.example.websocketdemo.service.UserService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
 import javax.servlet.http.Cookie;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doReturn;
+import static org.springframework.http.HttpStatus.FOUND;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -41,70 +32,76 @@ public class JwtAuthenticationFilterTests {
     @MockBean
     private JwtTokenProvider jwtTokenProvider;
 
-
+    private final String REQUEST_URL = "/api/v1/chatRooms";
+    private final String REDIRECT_URL = "/login";
 
     @Test
-    void chatRoomListPageBeforeLogin() throws  Exception{
+    void beforeLogin() throws Exception {
         //given
         given(jwtTokenProvider.resolveToken(any()))
                 .willReturn(null);
 
-
         //when
-        ResultActions ra = mvc.perform(get("/chatRoomList"));
+        MvcResult mvcResult = mvc.perform(get(REQUEST_URL))
+                                 .andReturn();
 
         //then
-        MvcResult mvcResult = ra.andExpect(status().is3xxRedirection()).andReturn();
-        assertThat(mvcResult.getResponse().getRedirectedUrl()).isEqualTo("/login");
-
+        assertThat(mvcResult.getResponse().getStatus()).isEqualTo(FOUND.value());
+        assertThat(mvcResult.getResponse().getRedirectedUrl()).isEqualTo(REDIRECT_URL);
     }
 
     @Test
-    void chatRoomListPageAfterLogin() throws  Exception{
+    void afterLogin() throws Exception {
         //given
-
         String accessToken = "test";
         given(jwtTokenProvider.resolveToken(any()))
                 .willReturn(anyString());
         given(jwtTokenProvider.validateToken(accessToken))
                 .willReturn(true);
         given(jwtTokenProvider.getAuthentication(any()))
-                .willReturn(any(Authentication.class));
+                .willReturn(new UsernamePasswordAuthenticationToken("test@naver.com", null, null));
 
-        //when
-        ResultActions ra = mvc.perform(get("/chatRoomList")
-                .header("Authorization", accessToken));
-
-        //then
-        MvcResult mvcResult = ra.andExpect(status().is3xxRedirection()).andReturn();
-        assertThat(mvcResult.getResponse().getRedirectedUrl()).isEqualTo("/login");
-
+        //when, then
+        mvc.perform(get(REQUEST_URL)
+           .header("Authorization", accessToken))
+           .andExpect(status().isOk());
     }
-    @Test
-    void chatRoomListPageWithOnlyRefreshToken() throws  Exception{
-        //given
 
+    @Test
+    void chatRoomListPageWithOnlyRefreshToken() throws Exception {
+        //given
         String refreshToken = "test";
-        String email = "test@naver.com";
         given(jwtTokenProvider.resolveToken(any()))
                 .willReturn(null);
         given(refreshTokenService.findByToken(anyString()))
-                .willReturn(any(RefreshToken.class));
-        given(refreshTokenService.verifyExpiration(RefreshToken.create(email)))
+                .willReturn(new RefreshToken(refreshToken));
+        given(jwtTokenProvider.validateToken(anyString()))
+                .willReturn(true);
+
+        //when, then
+        mvc.perform(get("/chatRoomList")
+           .cookie(new Cookie("refreshToken", refreshToken)))
+           .andExpect(status().isOk());
+    }
+
+    @Test
+    void otherPageWithOnlyRefreshToken() throws Exception {
+        //given
+        String refreshToken = "test";
+        given(jwtTokenProvider.resolveToken(any()))
+                .willReturn(null);
+        given(refreshTokenService.findByToken(anyString()))
+                .willReturn(new RefreshToken(refreshToken));
+        given(jwtTokenProvider.validateToken(anyString()))
                 .willReturn(true);
 
         //when
-        ResultActions ra = mvc.perform(get("/chatRoomList")
-                .cookie(new Cookie("refreshToken", refreshToken)));
+        MvcResult mvcResult = mvc.perform(get("/api/v1/chatRooms")
+                                 .cookie(new Cookie("refreshToken", refreshToken)))
+                                 .andReturn();
 
         //then
-        MvcResult mvcResult = ra.andExpect(status().is3xxRedirection()).andReturn();
-        assertThat(mvcResult.getResponse().getRedirectedUrl()).isEqualTo("/login");
-
+        assertThat(mvcResult.getResponse().getStatus()).isEqualTo(FOUND.value());
+        assertThat(mvcResult.getResponse().getRedirectedUrl()).isEqualTo(REDIRECT_URL);
     }
-
-
-
-
-
 }
