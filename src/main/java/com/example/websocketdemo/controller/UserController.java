2890b746d042dbff06afe6d96768c0186e8eddeb
hayoung <>
2022-06-07 23:12:25 +0900

    refactor: user 초기 데이터 생성 방법 변경

--- src/main/java/com/example/websocketdemo/controller/UserController.java
+++ src/main/java/com/example/websocketdemo/controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import javax.annotation.PostConstruct;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -99,13 +98,5 @@ public class UserController {
         LocalDateTime expiration = jwtTokenProvider.getTokenExpiration(accessToken);
         TokenResponse responseToken = new TokenResponse(accessToken, expiration);
         return new ResponseEntity<>(responseToken, HttpStatus.OK);
-    }
-
-
-    //테스트용 데이터 추가
-    @PostConstruct
-    public void init() {
-        userService.save("test@naver.com", "zns9dyek951956");
-        userService.save("test2@naver.com", "zns9dyek951956");
     }
 }
