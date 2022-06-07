fd7cd3031d67c55385dd1ee8e6f347542de1e678
hayoung <>
2022-06-07 20:25:37 +0900

    refactor: 목 데이터 생성 방법 변경

--- src/main/java/com/example/websocketdemo/controller/ChatRoomController.java
+++ src/main/java/com/example/websocketdemo/controller/ChatRoomController.java
@@ -5,7 +5,6 @@ import com.example.websocketdemo.service.ChatRoomService;
 import com.example.websocketdemo.service.dto.ChatRoomSaveRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 @RequestMapping("api/v1/chatRooms")
@@ -27,14 +26,5 @@ public class ChatRoomController {
     @ResponseBody
     public void addChatRoom(@RequestBody ChatRoomSaveRequest chatRoomSaveRequest) {
         chatRoomService.save(chatRoomSaveRequest.getName());
-    }
-
-    //테스트용 데이터 추가
-    @PostConstruct
-    public void init() {
-        chatRoomService.save("test1");
-        chatRoomService.save("test2");
-        chatRoomService.save("test3");
-        chatRoomService.save("test4");
     }
 }
---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,4 @@
+insert into CHAT_ROOM(id, name, count, expiry_date) values('55c9b6c7-53d8-4159-ad91-e9a9a018dd44', 'test1', 0, '2022-06-07 23:16:52.699205');
+insert into CHAT_ROOM(id, name, count, expiry_date) values('a86fa9ad-fada-49f9-b769-2ef480489f12', 'test2', 0, '2022-06-07 23:16:52.699205');
+insert into CHAT_ROOM(id, name, count, expiry_date) values('5961d302-a59d-4900-ab3f-c2e640250329', 'test3', 0, '2022-06-07 23:16:52.699205');
+insert into CHAT_ROOM(id, name, count, expiry_date) values('942f0927-0c83-4ae8-9599-5ac2b567fca0', 'test4', 0, '2022-06-07 23:16:52.699205');
